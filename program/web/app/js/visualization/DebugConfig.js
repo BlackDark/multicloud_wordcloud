4d1101e5aa6cf7f05978794d0fa5e115f488742d
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-08-04 20:27:52 +0200

    Update debug config

--- program/web/app/js/visualization/DebugConfig.js
+++ program/web/app/js/visualization/DebugConfig.js
@@ -1,9 +1,25 @@
 export default class DebugConfig {
 	constructor() {
 		this._collisionRemovalTimes = 200;
+		this._drawLinksD3Force = false;
+		this._drawLinksColaForce = false;
+		this._applyBorderConstraints = true;
 	}
 
 	get collisionRemovalTimes() {
 		return this._collisionRemovalTimes;
 	}
+
+	get drawLinksD3Force() {
+		return this._drawLinksD3Force;
+	}
+
+	get drawLinksColaForce() {
+		return this._drawLinksColaForce;
+	}
+
+	get applyBorderConstraints() {
+		return this._applyBorderConstraints;
+	}
+
 }
