441bb5e95dc1a1223619e038d9d1aac579ed9596
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-08-06 01:53:20 +0200

    Implement word element

--- program/web/app/js/visualization/elements/WordElement.js
+++ program/web/app/js/visualization/elements/WordElement.js
@@ -1,5 +1,23 @@
-export default class WordElement {
-	constructor() {
+import BaseElement from "js/visualization/elements/BaseElement";
 
+export default class WordElement extends BaseElement{
+	constructor(id) {
+		super(id);
+	}
+
+	draw(container) {
+		super.draw(container);
+
+		let color = d3.scale.category10();
+
+		container.append("text")
+			.classed("word-text", true)
+			.style("fill", function(d, i) { return color(i); })
+			//.attr("text-anchor", "middle")
+			.style("font-size", function(d) {
+				return d.size + "px"; })
+			.text(function(d) {
+				return d.text;
+			});
 	}
 }
