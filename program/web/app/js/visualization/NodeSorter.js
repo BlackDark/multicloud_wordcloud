a95ec16bbb7b93996f1be7179ea63b5e30a5b229
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-08-09 20:16:43 +0200

    Implement the node sorter

--- program/web/app/js/visualization/NodeSorter.js
+++ program/web/app/js/visualization/NodeSorter.js
@@ -6,28 +6,75 @@ export default class NodeSorter {
 		this._endPoints = endPoints;
 		this._skippedNodes = [];
 		this._placedNodes = [];
+		this._roundIndex = 0;
+		this._sortingNodes();
 	}
 
 	hasNodes() {
-		// TODO
+		for(let key of this._endToNode.keys()) {
+			if(this._endToNode.get(key).length !== 0) {
+				return true;
+			}
+		}
+
 		return false;
 	}
 
 	getNodeForEndPoints(endpoint) {
-		// TODO does not remove from array
-		return null;
+		return this._endToNode.get(endpoint)[0];
 	}
 
 	placeNode(node) {
-
+		this._placedNodes.push(node);
+		this._removeNodeFromLists(node);
 	}
 
 	skipNode(node) {
+		this._skippedNodes.push(node);
+		this._removeNodeFromLists(node);
+	}
 
+	_removeNodeFromLists(node) {
+		for(let key of this._endToNode.keys()) {
+			let array = this._endToNode.get(key);
+			array = array.splice(array.indexOf(node), 1);
+		}
 	}
 
 	// Iterate through the endpoint and give node back
-	getNextNode() {
+	getNextNodeAndEndPoint() {
+		var endPoint = this._endPoints[this._roundIndex];
+		let node = this._endToNode.get(endPoint)[0];
 
+		if(this._roundIndex === this._endPoints.length - 1) {
+			this._roundIndex = 0;
+		} else {
+			this._roundIndex++;
+		}
+
+		return {
+			"node": node,
+			"endPoint": endPoint
+		};
 	}
+
+	_sortingNodes() {
+		this._endToNode = new Map();
+
+		this._endPoints.forEach(node => {
+			this._endToNode.set(node, sortNodesToPoint(node, this._nodes));
+		});
+	}
+}
+
+function sortNodesToPoint(point, nodes) {
+	let unsorted = [].concat(nodes);
+
+	unsorted.sort(function (a, b) {
+		let aDistance = Math.sqrt(Math.pow(a.x - point.x, 2) + Math.pow(a.y - point.y, 2));
+		let bDistance = Math.sqrt(Math.pow(b.x - point.x, 2) + Math.pow(b.y - point.y, 2));
+		return aDistance - bDistance;
+	});
+
+	return unsorted;
 }
