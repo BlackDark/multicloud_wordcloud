376d867f5396873e2f6ba0bc6fffcb414245cbce
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-09-07 18:47:24 +0200

    Do not remove all dom elements when cleaning visualization

--- program/backend/src/main/web/app/js/visualization/VisualzationInitializer.js
+++ program/backend/src/main/web/app/js/visualization/VisualzationInitializer.js
@@ -24,7 +24,7 @@ export default class VisualzationInitializer {
 	}
 
 	cleanVisualization() {
-		$(this._graphSelector).empty();
+		$(this._graphSelector).find('svg').empty();
 	}
 
 	executeSample() {
