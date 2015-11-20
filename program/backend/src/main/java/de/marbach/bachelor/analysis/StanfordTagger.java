14ac1145070ec4016ea2548b14b605c66875bfaa
Eduard Marbach <marbaced@studi.informatik.uni-stuttgart.de>
2015-11-20 18:32:41 +0100

    Remove unused code from stanford

--- program/backend/src/main/java/de/marbach/bachelor/analysis/StanfordTagger.java
+++ program/backend/src/main/java/de/marbach/bachelor/analysis/StanfordTagger.java
@@ -43,53 +43,6 @@ public class StanfordTagger {
 		pipeline = new StanfordCoreNLP(props);
 	}
 
-	public static void main(String[] args) throws Exception {
-		// creates a StanfordCoreNLP object, with POS tagging, lemmatization, NER, parsing, and coreference resolution
-		Properties props = new Properties();
-		props.setProperty("annotators", "tokenize, ssplit, pos, lemma");
-		StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-
-		// read some text in the text variable
-		String text = "Test";
-
-		// create an empty Annotation just with the given text
-		Annotation document = new Annotation(text);
-
-		// run all Annotators on this text
-		pipeline.annotate(document);
-
-		// these are all the sentences in this document
-		// a CoreMap is essentially a Map that uses class objects as keys and has values with custom types
-		List<CoreMap> sentences = document.get(CoreAnnotations.SentencesAnnotation.class);
-
-		for (CoreMap sentence : sentences) {
-			// traversing the words in the current sentence
-			// a CoreLabel is a CoreMap with additional token-specific methods
-			for (CoreLabel token : sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-				// this is the text of the token
-				String word = token.get(CoreAnnotations.TextAnnotation.class);
-				// this is the POS tag of the token
-				String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-				String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
-
-				System.out.println(word + "\n\t" + pos + "\n\t" + lemma);
-			}
-
-			// this is the parse tree of the current sentence
-			Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-
-			// this is the Stanford dependency graph of the current sentence
-			SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
-		}
-
-		// This is the coreference link graph
-		// Each chain stores a set of mentions that link to each other,
-		// along with a method for getting the most representative mention
-		// Both sentence and token offsets start at 1!
-		Map<Integer, CorefChain> graph =
-				document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
-	}
-
 	public void annotate(File file, Map<String, NodeElement> allNodes) throws FileNotFoundException {
 		String content = new Scanner(file).useDelimiter("\\Z").next();
 
@@ -118,20 +71,7 @@ public class StanfordTagger {
 					nodeElement.addTag(pos);
 				}
 			}
-
-			// this is the parse tree of the current sentence
-			Tree tree = sentence.get(TreeCoreAnnotations.TreeAnnotation.class);
-
-			// this is the Stanford dependency graph of the current sentence
-			SemanticGraph dependencies = sentence.get(SemanticGraphCoreAnnotations.CollapsedCCProcessedDependenciesAnnotation.class);
 		}
-
-		// This is the coreference link graph
-		// Each chain stores a set of mentions that link to each other,
-		// along with a method for getting the most representative mention
-		// Both sentence and token offsets start at 1!
-		Map<Integer, CorefChain> graph =
-				document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
 	}
 
 }
