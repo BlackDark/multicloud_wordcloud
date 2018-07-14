f94003456348bfa59d38b23bf2a1b6e4135a0b99
Eduard Marbach <mail@eduard-marbach.de>
2018-07-15 00:02:42 +0200

    Fix problem with double values in helper method _createArray

--- src/main/web/app/js/visualization/util/GeneratorUtil.js
+++ src/main/web/app/js/visualization/util/GeneratorUtil.js
@@ -7,12 +7,13 @@ export default class GeneratorUtil {
 	// Depending on parameters multi-dimensional arrays are created.
 	// Eq. (3) creates [0,0,0] and (3,2) creates [ [0,0], [0,0], [0,0]] etc.
 	static _createArray(length) {
-		let arr = new Array(length || 0),
-			i = length;
+		let lengthAsInteger = parseInt(length, 10);
+		let arr = new Array(lengthAsInteger || 0);
+		let i = lengthAsInteger;
 
 		if (arguments.length > 1) {
 			let args = Array.prototype.slice.call(arguments, 1);
-			while (i--) arr[length - 1 - i] = GeneratorUtil._createArray.apply(this, args);
+			while (i--) arr[lengthAsInteger - 1 - i] = GeneratorUtil._createArray.apply(this, args);
 		}
 
 		return arr;
